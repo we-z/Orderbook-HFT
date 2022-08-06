415ffa3e173dbcda5d5f2c55a32d78a3d616ea90
Wheezy <32607367+we-z@users.noreply.github.com>
2022-08-06 22:22:49 +0200

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,38 @@
+import alpaca_trade_api as tradeapi
+import config
+import time
+
+opts = {}
+opts['key_id'] = config.API_KEY
+opts['secret_key'] = config.SECRET_KEY
+opts['base_url'] = config.BASE_URL
+
+# API object used to submit orders
+api = tradeapi.REST(**opts)
+# Data streaming connection
+conn = tradeapi.Stream(**opts)
+
+t = True
+symbol = 'AAPL'
+@conn.on_quote(symbol)
+async def on_quote(data):
+    global t
+    if t:
+        if data.bid_size > (data.ask_size * 4):
+            api.submit_order(symbol, 100, "buy", 
+                                "market", "day")
+            print('Buy at', data.ask_price)
+            t = False
+        if data.ask_size > (data.bid_size * 4):
+            api.submit_order(symbol, 100, "sell", 
+                                "market", "day")
+            print('Sell at', data.bid_price)
+            t = False
+    if data.ask_size == data.bid_size and not t:
+        api.close_all_positions()
+        print('Close at BID', data.bid_price, 
+                        'ASK', data.ask_price)
+        time.sleep(1)
+        t = True
+
+conn.run()
