24e00804fe89857b01c8b44c274f66eb580340c2
Wheezy <32607367+we-z@users.noreply.github.com>
2022-08-12 09:12:44 -0700

    Strategy update
    limit orders and buy positions only.

--- simple.py
+++ simple.py
@@ -1,40 +1,47 @@
 import alpaca_trade_api as tradeapi
 import config
-import time
-
-opts = {}
-opts['key_id'] = config.API_KEY
-opts['secret_key'] = config.SECRET_KEY
-opts['base_url'] = config.BASE_URL
 
 # API object used to submit orders
-api = tradeapi.REST(**opts)
+api = tradeapi.REST(
+        key_id=config.API_KEY,
+        secret_key=config.SECRET_KEY,
+        base_url=config.BASE_URL
+)
+
 # Data streaming connection
-conn = tradeapi.Stream(**opts)
+conn = tradeapi.Stream(
+        key_id=config.API_KEY,
+        secret_key=config.SECRET_KEY,
+        base_url=config.BASE_URL
+)
 
-t = True
-symbol = 'AAPL'
-@conn.on_quote(symbol)
+# Subscribe to Live Order Book updates
+@conn.on_quote('SPY')
 async def on_quote(data):
-    global t
-    if t:
-        if data.bid_size > (data.ask_size * 4):
-            api.submit_order(symbol, 100, "buy", 
-                                "market", "day")
-            print('Buy at BID', data.bid_price,
-                        'ASK', data.ask_price)
-            t = False
-        if data.ask_size > (data.bid_size * 4):
-            api.submit_order(symbol, 100, "sell", 
-                                "market", "day")
-            print('Sell at BID', data.bid_price,
-                        'ASK', data.ask_price)
-            t = False
-    if data.ask_size == data.bid_size and not t:
-        api.close_all_positions()
-        print('Close at BID', data.bid_price, 
-                        'ASK', data.ask_price)
-        time.sleep(1)
-        t = True
+    
+    # Buying the Ask when there are more sellers
+    if (
+        data.ask_size > (data.bid_size * 1) 
+        and float(api.get_account().cash) > data.ask_price
+    ):
+        o = api.submit_order(
+            'SPY',1,"buy","limit","day", 
+            limit_price=str(data.ask_price)
+        )
+        api.cancel_order(o.id)
+        print('Buying at ASK', data.ask_price)
+    
+    # Selling to the Bid wheb there are more buyers
+    if (
+        data.bid_size > (data.ask_size * 1) 
+        and api.list_positions()
+    ):
+        o = api.submit_order(
+            'SPY',1,"sell","limit","day", 
+            limit_price=str(data.bid_price)
+        )
+        api.cancel_order(o.id)
+        print('Selling at BID', data.bid_price) 
 
+# Go Live
 conn.run()
